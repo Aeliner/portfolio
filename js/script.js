751868bdf360f8c6179e023bee07a34f0e0d065d
Nazareno <Aeliner123@gmail.com>
2020-08-31 19:10:03 -0300

    Finished making animations for projects and contact section

--- js/script.js
+++ js/script.js
@@ -75,7 +75,38 @@ new fullpage("#container", {
         img.style.animationDelay = delay + "s";
       }
     } else if (index == 2) {
-      //projects
+      let content = document.getElementById("projectsContent");
+      content.style.display = "block";
+      let h3 = document.getElementById("h3Projects");
+      h3.classList.add("animate__animated",
+      "animate__fadeIn", "delay-05");
+      let lArrow = document.getElementById("prev");
+      let rArrow = document.getElementById("next");
+      let controls = document.getElementById("controlsDiv");
+      controls.classList.add("animate__animated", "animate__fadeIn", "animate__delay-1s");
+      lArrow.classList.add("leftArrow","animate__animated", "animate__shakeX", "animate__delay-2s");
+      rArrow.classList.add("rightArrow","animate__animated", "animate__shakeX", "animate__delay-2s");
+
+      let project1 = document.getElementById("project1");
+      let project2= document.getElementById("project2");
+      let project3= document.getElementById("project3");
+      let project4=document.getElementById("project4");
+      let project5=document.getElementById("project5");
+
+      project1.classList.add("animate__animated", "animate__fadeInUp", "delay-125");
+      project2.classList.add("animate__animated", "animate__fadeInUp", "delay-15");
+      project3.classList.add("animate__animated", "animate__fadeInUp", "delay-175");
+      project4.classList.add("animate__animated", "animate__fadeInUp", "delay-2");
+      project5.classList.add("animate__animated", "animate__fadeInUp", "delay-225");
+      
+      let sliderContainer = document.getElementById("sliderContainer");
+      sliderContainer.classList.add("animate__animated", "animate__fadeIn", "animate__delay-1s");
+      let sliderUl = document.getElementById("pagination-ul");
+      sliderUl.classList.add("animate__animated", "animate__fadeIn", "animate__delay-1s");
+
+      let h3Bottom = document.getElementById("projectsBottomH3");
+      h3Bottom.classList.add("animate__animated", "animate__fadeIn", "animate__delay-3s")
+
     } else {
       //contact
     }
